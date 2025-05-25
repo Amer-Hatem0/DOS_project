049c0f48a21176973dcb628bf5d785cd21ba1f0b
RashidSawalha <116581983+RashidSawalha@users.noreply.github.com>
2025-05-25 17:11:35 +0300

    Update purchase.py

--- purchase/purchase.py
+++ purchase/purchase.py
@@ -1,46 +1,46 @@
 import sqlite3
-import requests as requests
+import requests
 from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 def get_db_connection():
     conn = sqlite3.connect('/data/order.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-app = Flask(__name__)
-
 @app.route('/purchase/<item_num>', methods=['POST'])
 def orders(item_num):
-    base_url = 'http://172.17.0.1:5050/query'
-
-    response = requests.get(base_url, params={'item_number': item_num})
+ 
+    response = requests.get("http://catalog1:5000/query", params={'item_number': item_num})
 
     if response.ok and response.status_code == 200:
         data = response.json()
 
         if data['quantity'] <= 0:
-            return jsonify({'message': "this book is out of stock"}), 406
+            return jsonify({'message': "This book is out of stock"}), 406
 
-        response = requests.patch('http://172.17.0.1:5050/update', json={
+        response = requests.patch("http://catalog1:5000/update", json={
             "count": -1,
-            "itemNumber": item_num})
+            "itemNumber": item_num
+        })
 
         patch_response = response.json()
-        print(patch_response)
-
-        if 'message' in patch_response and patch_response['message'] == f"Updated record {item_num} successfully":
-            new_count = patch_response.get('new_count', 'N/A')
-            new_cost = patch_response.get('new_cost', 'N/A')
-
+        if 'message' in patch_response and patch_response['message'].startswith("Updated record"):
             con = get_db_connection()
             con.cursor().execute("INSERT INTO 'order' (ItemNumber) VALUES (?)", (item_num,))
             con.commit()
 
-            return jsonify({'message': f'successfully purchased item {item_num}'}), 200
-        else:
-            return jsonify({'message': f'failed to Purchase item {item_num}'}), 404
-    else:
-        return jsonify({'message': f'failed to Purchase item {item_num}'}), 404
+            #  Invalidate cache in front-end
+            for endpoint in ["http://front_api:5000", "http://localhost:8080"]:
+                try:
+                    requests.post(f"{endpoint}/invalidate/{item_num}")
+                except Exception as e:
+                    print(f"[Warning] Failed to notify frontend: {e}")
+
+            return jsonify({'message': f'Successfully purchased item {item_num}'}), 200
+
+    return jsonify({'message': f'Failed to purchase item {item_num}'}), 404
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
